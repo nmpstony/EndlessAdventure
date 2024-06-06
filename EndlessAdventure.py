f39695c195ba62c34e57692656771d71ca423653
Nguyen Minh Phu <phunguyen15102005@gmail.com>
2024-06-06 19:13:44 +0700

    Shield and extra life

--- EndlessAdventure.py
+++ EndlessAdventure.py
@@ -1,6 +1,7 @@
 import pygame as pg
+import random
 from config import *
-from _init__ import Character,Obstacle,screen,clock
+from _init__ import Character,GameObject,screen,clock,obj_size
 
 def draw_bg():
     """
@@ -10,21 +11,47 @@ def draw_bg():
     and draws two ground images that move leftwards to create a continuous scrolling effect.
     Resets the ground position when the second ground image goes off-screen.-
     """
-    screen.fill(BG)
     global ground1_x, ground2_x
     screen.blit(bg,(0,0))
-    screen.blit(ground,(ground1_x,HEIGHT - 64))
-    screen.blit(ground,(ground2_x,HEIGHT - 64))
+    screen.blit(GROUND,(ground1_x,HEIGHT - 64))
+    screen.blit(GROUND,(ground2_x,HEIGHT - 64))
     score_txt = font.render(f'Distance: {player.score}m', True, (255,255,255))
     score_rect = score_txt.get_rect(center = (WIDTH//2, 30))
     screen.blit(score_txt,score_rect)
-    skyjump_txt = font.render(f'Skyjump: {player.skyjump}', True, (255,255,255))
+    skyjump_txt = font.render(f'Skyjump: {player.temp_skyjump}', True, (255,255,255))
     screen.blit(skyjump_txt,(10,15))
     ground1_x -=speed
     ground2_x -=speed
     if ground2_x <= 0:
         ground1_x = 0
         ground2_x = WIDTH
+
+# Create game object
+def create_game_obj():
+    global game_objects
+    temp_x, temp_y =-1,-1
+    for _ in range(5):
+        game_object = GameObject("obstacle")
+        while True:
+            if game_object.rect.x == temp_x and game_object.rect.y == temp_y:
+                game_object = GameObject("obstacle")
+                continue
+            else:
+                break
+        temp_x, temp_y = game_object.rect.x, game_object.rect.y
+        game_objects.add(game_object)
+
+    for _ in range(4):
+        orb_type = random.choice(["shield", "extra_life"])
+        orb = GameObject(orb_type)
+        while True:
+            if orb.rect.x == temp_x and orb.rect.y == temp_y:
+                orb = GameObject(orb_type)
+                continue
+            else:
+                break
+        temp_x, temp_y = orb.rect.x, orb.rect.y
+        game_objects.add(orb)
 
 # Initialize Pygame
 pg.init()
@@ -36,22 +63,25 @@ clock = pg.time.Clock()
 ground1_x = 0
 ground2_x = WIDTH
 jump = False
+count = 0
+
 
 # Load and scale images
 bg=pg.transform.scale(pg.image.load(r'Image/background.png'),(WIDTH, HEIGHT))
-ground=pg.transform.scale(pg.image.load(r'Image/ground.png'),(WIDTH, 64))
+GROUND=pg.transform.scale(pg.image.load(r'Image/ground.png'),(WIDTH, 64))
+SHIELD=pg.transform.scale(pg.image.load(r'Image/shield_effect.png'),(obj_size*4,obj_size*4))
+SHIELD_RECT=SHIELD.get_rect()
 
 # Load font
 font = pg.font.SysFont('sans',30)
 
 # Create a player character
 player = Character(250,295)
-obstacles = pg.sprite.Group()
-for _ in range(5):
-    obstacle = Obstacle()
-    obstacles.add(obstacle)
 
-all_sprites = pg.sprite.Group(player, *obstacles)
+# Create game objects
+game_objects = pg.sprite.Group()
+create_game_obj()
+all_sprites = pg.sprite.Group(player, *game_objects)
 
 while True:
     """
@@ -71,11 +101,30 @@ while True:
     player.jump(jump)
     
     # Update
+    if len(game_objects)<=4:
+        create_game_obj()
+        all_sprites = pg.sprite.Group(player, *game_objects)
+
     all_sprites.update()
 
     # Check for collisions
-    if pg.sprite.spritecollide(player, obstacles, False):
-        break
+    for obj in pg.sprite.spritecollide(player, game_objects, False):
+        if obj.type == "obstacle":
+            if player.shield:
+                player.shield = False  # Use the shield
+            elif player.lives > 1:
+                player.lives -= 1  # Lose a life
+            else:
+                pg.quit()
+                exit()  # Game over if no shield and no extra life
+            obj.kill()
+        elif obj.type == "shield":
+            player.shield = True
+            obj.kill()  # Remove the orb
+        elif obj.type == "extra_life":
+            player.lives += 1
+            obj.kill()  # Remove the orb
+
     all_sprites.draw(screen)
 
     if player.velocity < 0:
@@ -93,13 +142,11 @@ while True:
         # Keyboard presses
         if event.type == pg.KEYDOWN:
             if (event.key == pg.K_SPACE or event.key == pg.K_UP):
-                if player.temp_skyjump > 1:
-                    player.temp_skyjump -= 1
+                if player.temp_skyjump > 0:
                     jump = True
 
-        ## Keyboard released
-        #if event.type == pg.KEYUP:
-        #    if event.key == pg.K_SPACE or event.key == pg.K_UP:
-        #        jump = False
+    if player.shield:
     
+        SHIELD_RECT.center=(player.rect.center)
+        screen.blit(SHIELD, SHIELD_RECT)
     pg.display.update()
--- _init__.py
+++ _init__.py
@@ -8,9 +8,13 @@ screen = pg.display.set_mode((WIDTH,HEIGHT))
 clock = pg.time.Clock()
 
 # Load and scale images
-ob_size = pg.image.load(f'animation/run/0.png').get_height()//1.5
+obj_size = pg.image.load(f'animation/run/0.png').get_height()//1.5
 OBSTACLE_IMG = pg.image.load("Image/bomb.png")
-OBSTACLE_IMG = pg.transform.scale(OBSTACLE_IMG, (ob_size,ob_size))
+OBSTACLE_IMG = pg.transform.scale(OBSTACLE_IMG, (obj_size,obj_size))
+SHIELD_ORB_IMG = pg.image.load("Image/shield.png")
+SHIELD_ORB_IMG = pg.transform.scale(SHIELD_ORB_IMG, (obj_size,obj_size))
+EXTRA_LIFE_ORB_IMG = pg.image.load("Image/extra_life.png")
+EXTRA_LIFE_ORB_IMG = pg.transform.scale(EXTRA_LIFE_ORB_IMG, (obj_size,obj_size))
 
 class Character(pg.sprite.Sprite):
     """
@@ -20,16 +24,14 @@ class Character(pg.sprite.Sprite):
     ----------
     velocity : int
         The current vertical velocity of the character.
-    animation_list : list
+    __animation_list : list
         A list of lists containing the animation frames for different actions.
-    frame_index : int
+    __frame_index : int
         The current frame index of the animation.
-    action : int
-        The current action of the character (e.g., running, jumping, falling).
-    update_time_animation : int
+    __action : int
+        The current __action of the character (e.g., running, jumping, falling).
+    __update_time_animation : int
         The time (in milliseconds) when the animation was last updated.
-    update_time_jump : int
-        The time (in milliseconds) when the jump was last updated.
     image : pygame.Surface
         The current image/frame of the character.
     rect : pygame.Rect
@@ -44,9 +46,9 @@ class Character(pg.sprite.Sprite):
     jump(jump)
         Makes the character jump if the jump parameter is True.
     update_animation()
-        Updates the character's animation based on the current action.
+        Updates the character's animation based on the current __action.
     update_action(new_action)
-        Updates the character's action and resets the animation if the action changes.
+        Updates the character's __action and resets the animation if the __action changes.
     draw()
         Draws the character on the screen.
     """
@@ -68,29 +70,30 @@ class Character(pg.sprite.Sprite):
         self.score = 0
         self.skyjump = 3
         self.temp_skyjump = self.skyjump
-        self.animation_list = []
-        self.frame_index = 0
-        self.action = 1
-        self.update_time_animation = pg.time.get_ticks()
-        self.update_time_score = pg.time.get_ticks()
-        self.update_time_jump = pg.time.get_ticks()
+        self.shield = False  # Shield attribute
+        self.lives = 1  # Lives attribute
+        self.__animation_list = []
+        self.__frame_index = 0
+        self.__action = 1
+        self.__update_time_animation = pg.time.get_ticks()
+        self.__update_time_score = pg.time.get_ticks()
         temp_list = []
 
         for i in range(6):
             img = pg.image.load(f'animation/run/{i}.png')
             img = pg.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
             temp_list.append(img)
-        self.animation_list.append(temp_list)
+        self.__animation_list.append(temp_list)
 
         img = pg.image.load(f'animation/jump/1.png')
         img = pg.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-        self.animation_list.append([img])
+        self.__animation_list.append([img])
 
         img = pg.image.load(f'animation/fall/0.png')
         img = pg.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-        self.animation_list.append([img])
+        self.__animation_list.append([img])
 
-        self.image = self.animation_list[self.action][self.frame_index]
+        self.image = self.__animation_list[self.__action][self.__frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
@@ -120,44 +123,46 @@ class Character(pg.sprite.Sprite):
             If True, the character will jump.
         """
         if jump:
-            self.velocity = -8
+            if self.rect.bottom < HEIGHT-50:
+                self.temp_skyjump -= 1
+            self.velocity = -7
 
     def update_animation(self):
         """
-        Updates the character's animation frame based on the current action and elapsed time.
+        Updates the character's animation frame based on the current __action and elapsed time.
         """
         ANIMATION_COOLDOWN = 150
         #update imgae depending on current frame
-        self.image = self.animation_list[self.action][self.frame_index]
+        self.image = self.__animation_list[self.__action][self.__frame_index]
         #chech if enough time has passed since the last update
-        if pg.time.get_ticks() - self.update_time_animation > ANIMATION_COOLDOWN:
-            self.update_time_animation = pg.time.get_ticks()
-            self.frame_index += 1
+        if pg.time.get_ticks() - self.__update_time_animation > ANIMATION_COOLDOWN:
+            self.__update_time_animation = pg.time.get_ticks()
+            self.__frame_index += 1
         #if the animation has run our the reset back to the start
-        if self.frame_index >= len(self.animation_list[self.action]):
-            self.frame_index = 0
+        if self.__frame_index >= len(self.__animation_list[self.__action]):
+            self.__frame_index = 0
     
     def update_score(self):
         SCORE_COOLDOWN = 300
-        if pg.time.get_ticks() - self.update_time_score > SCORE_COOLDOWN:
-            self.update_time_score = pg.time.get_ticks()
+        if pg.time.get_ticks() - self.__update_time_score > SCORE_COOLDOWN:
+            self.__update_time_score = pg.time.get_ticks()
             self.score +=1
 
     def update_action(self, new_action):
         """
-        Updates the character's action and resets the animation if the action changes.
+        Updates the character's __action and resets the animation if the __action changes.
 
         Parameters
         ----------
         new_action : int
-            The new action for the character.
+            The new __action for the character.
         """
-        #check if the new action is different to the pervious one
-        if new_action != self.action:
-            self.action = new_action
+        #check if the new __action is different to the pervious one
+        if new_action != self.__action:
+            self.__action = new_action
             #update the animation settings
-            self.frame_index = 0
-            self.update_time_animation = pg.time.get_ticks()
+            self.__frame_index = 0
+            self.__update_time_animation = pg.time.get_ticks()
 
     def draw(self):
         """
@@ -165,16 +170,54 @@ class Character(pg.sprite.Sprite):
         """
         screen.blit(self.image, self.rect)
 
-class Obstacle(pg.sprite.Sprite):
-    def __init__(self):
+class GameObject(pg.sprite.Sprite):
+    """
+    A class to represent various objects in the game, including obstacles and orbs.
+
+    Attributes
+    ----------
+    type : str
+        The type of the game object, which can be "obstacle", "shield", or "extra_life".
+    image : pygame.Surface
+        The image representing the game object.
+    rect : pygame.Rect
+        The rectangular area representing the game object's position and size.
+
+    Methods
+    -------
+    __init__(obj_type)
+        Initializes the game object with a specific type.
+    update()
+        Updates the position of the game object, moving it leftwards across the screen.
+    """
+    def __init__(self, obj_type):
+        """
+        Initializes the game object with a specific type.
+
+        Parameters
+        ----------
+        obj_type : str
+            The type of the game object, which determines its appearance and behavior.
+            Possible values are "obstacle", "shield", and "extra_life".
+        """
         super().__init__()
-        self.image = OBSTACLE_IMG
+        self.type = obj_type
+        if self.type == "obstacle":
+            self.image = OBSTACLE_IMG
+        elif self.type == "shield":
+            self.image = SHIELD_ORB_IMG
+        elif self.type == "extra_life":
+            self.image = EXTRA_LIFE_ORB_IMG
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(WIDTH, 2*WIDTH)
+        self.rect.x = random.randint(WIDTH, 3 * WIDTH)
         self.rect.y = random.randint(0, HEIGHT - self.rect.height - 60)
 
     def update(self):
+        """
+        Updates the position of the game object, moving it leftwards across the screen.
+        Resets the object's position when it moves off the left side of the screen.
+        """
         self.rect.x -= speed
         if self.rect.right < 0:
-            self.rect.x = random.randint(WIDTH, 2*WIDTH)
+            self.rect.x = random.randint(WIDTH, 3 * WIDTH)
             self.rect.y = random.randint(0, HEIGHT - self.rect.height - 60)
